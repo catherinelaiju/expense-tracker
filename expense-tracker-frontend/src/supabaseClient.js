2c81b6a910b20e9b7e917b0f913a385d77fcd59c
catherinelaiju <catherinelaiju04@gmail.com>
2025-09-12 00:58:20 +0530

    update supabaseClient.js to use env variables

--- expense-tracker-frontend/src/supabaseClient.js
+++ expense-tracker-frontend/src/supabaseClient.js
@@ -1,8 +1,6 @@
-// src/supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
 
-// ⬇️ Replace these with your actual Supabase project values
-const supabaseUrl = "https://ulfffbaaetrowenmkrph.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVsZmZmYmFhZXRyb3dlbm1rcnBoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcwNzg5MDcsImV4cCI6MjA3MjY1NDkwN30.RJZJMeuUb6tH6QMqu8xoPSwjJpTkQvXfSb78qOzJg8s";
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
